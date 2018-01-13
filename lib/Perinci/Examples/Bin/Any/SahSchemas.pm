eebc938acf3ac1be67038bb0a9d63d5ecfa6d2bf
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2018-01-13 18:38:59 +0700

    Initial commit

---
+++ lib/Perinci/Examples/Bin/Any/SahSchemas.pm
@@ -0,0 +1,18 @@
+package Perinci::Examples::Bin::Any::SahSchemas;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Example CLI scripts to test various Sah schemas
+
+=head1 DESCRIPTION
+
+This distribution contains the following scripts:
+
+# INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+Related C<Sah::Schema::*> modules
